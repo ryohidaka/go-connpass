2acab563b2a9257ce7e7856401f2e2a9648ab54a
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-04-23 09:10:26 +0900

    feat(api): 共通のリクエスト処理を追加

--- client.go
+++ client.go
@@ -1,9 +1,14 @@
 package connpass
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
+	"reflect"
 	"time"
+
+	"github.com/google/go-querystring/query"
 )
 
 // Connpass クライアント構造体
@@ -28,4 +33,56 @@ func NewClient(apiKey string) *Connpass {
 		APIVersion: APIVersion,
 		BaseURL:    fmt.Sprintf("%s/%s", BaseURL, APIVersion),
 	}
+}
+
+// 指定された Connpass API のエンドポイントに GET リクエストを送り、
+// その JSON レスポンスを指定された型 T にデコードして返却する。
+//
+// パラメータ:
+//   - endpoint: API の相対パス (例: "events")
+//   - query: クエリパラメータのマップ (キーに対して複数の値を指定可能)
+//
+// 戻り値:
+//   - レスポンスをデコードした型 T の値
+//   - エラーが発生した場合は error を返す
+func (c *Connpass) Request(endpoint string, queryStruct any, out any) error {
+	reqURL, err := url.Parse(fmt.Sprintf("%s/%s", c.BaseURL, endpoint))
+	if err != nil {
+		return fmt.Errorf("URLのパースに失敗しました: %w", err)
+	}
+
+	// クエリを構造体で受け取った場合のみエンコード
+	if queryStruct != nil && reflect.TypeOf(queryStruct).Kind() == reflect.Struct {
+		values, err := query.Values(queryStruct)
+		if err != nil {
+			return fmt.Errorf("クエリのエンコードに失敗しました: %w", err)
+		}
+		reqURL.RawQuery = values.Encode()
+	}
+
+	req, err := http.NewRequest("GET", reqURL.String(), nil)
+	if err != nil {
+		return fmt.Errorf("HTTPリクエストの作成に失敗しました: %w", err)
+	}
+	req.Header.Set("X-API-Key", c.APIKey)
+
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return fmt.Errorf("APIリクエストに失敗しました: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 && resp.StatusCode < 500 {
+		return fmt.Errorf("APIリクエストに失敗しました: %d %s", resp.StatusCode, http.StatusText(resp.StatusCode))
+	}
+
+	if resp.StatusCode >= 500 {
+		return fmt.Errorf("予期しないエラーが発生しました: %d %s", resp.StatusCode, http.StatusText(resp.StatusCode))
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(out); err != nil {
+		return fmt.Errorf("JSONデコードに失敗しました: %w", err)
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ryohidaka/go-connpass
 
 go 1.24.2
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/google/go-querystring v1.1.0
+	github.com/joho/godotenv v1.5.1
+)
