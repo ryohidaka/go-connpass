34a76b648f08f593cc6da8d8ac24df13828fe653
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-04-26 01:36:00 +0900

    test: GetUserPresenterEvents() のテストを追加

---
+++ user-presenter-events_test.go
@@ -0,0 +1,86 @@
+package connpass_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ryohidaka/go-connpass"
+	"github.com/ryohidaka/go-connpass/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserPresenterEvents(t *testing.T) {
+	// ダミーを生成
+	dummyQuery := testutil.DummyGetUserPresenterEventsQuery()
+	dummyResponse := testutil.DummyGetUserPresenterEventsResponse()
+
+	// モックサーバーを作成
+	t.Run("正常系", func(t *testing.T) {
+		// モックのHTTPサーバーを作成
+		mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// レスポンスを模擬する
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(dummyResponse)
+		}))
+		defer mockServer.Close()
+
+		// クライアント設定
+		c := connpass.NewClient("dummy-api-key")
+		c.BaseURL = mockServer.URL // モックサーバーのURLを設定
+
+		// ユーザー発表イベント取得
+		resp, err := c.GetUserPresenterEvents("haru860", &dummyQuery)
+
+		// レスポンスの確認
+		assert.NoError(t, err)
+		assert.Equal(t, dummyResponse, resp)
+	})
+
+	// 異常系テストケース
+	t.Run("異常系", func(t *testing.T) {
+		t.Run("APIエラー", func(t *testing.T) {
+			// モックのHTTPサーバーを作成
+			mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				// エラーレスポンスを模擬
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("Bad Request"))
+			}))
+			defer mockServer.Close()
+
+			// クライアント設定
+			c := connpass.NewClient("dummy-api-key")
+			c.BaseURL = mockServer.URL // モックサーバーのURLを設定
+
+			// ユーザー発表イベント取得
+			_, err := c.GetUserPresenterEvents("dummy-nickname", &dummyQuery)
+
+			// エラーチェック
+			assert.Error(t, err)
+			assert.Equal(t, "APIリクエストに失敗しました: 400 Bad Request", err.Error())
+		})
+
+		t.Run("予期しないエラーAPIエラー", func(t *testing.T) {
+			// モックのHTTPサーバーを作成
+			mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				// 予期しないエラーレスポンスを模擬
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("Internal Server Error"))
+			}))
+			defer mockServer.Close()
+
+			// クライアント設定
+			c := connpass.NewClient("dummy-api-key")
+			c.BaseURL = mockServer.URL // モックサーバーのURLを設定
+
+			// ユーザー発表イベント取得
+			_, err := c.GetUserPresenterEvents("dummy-nickname", &dummyQuery)
+
+			// エラーチェック
+			assert.Error(t, err)
+			assert.Equal(t, "予期しないエラーが発生しました: 500 Internal Server Error", err.Error())
+		})
+	})
+}
